a8b599865c67cea6da596da489388aa2603b2ab4
aksource <akira.mew@gmail.com>
2014-07-18 21:34:58 +0900

    Fixed player could dig bedrock placed y-0.

--- java/DigBedrock/DigBedrock.java
+++ java/DigBedrock/DigBedrock.java
@@ -1,11 +1,17 @@
 package DigBedrock;
 
-import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.Event;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 @Mod(modid="DigBedrock", name="DigBedrock", version="@VERSION@",dependencies="required-after:FML")
-//@NetworkMod(clientSideRequired=true, serverSideRequired=false)
 
 public class DigBedrock
 {
@@ -15,6 +21,19 @@ public class DigBedrock
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
-		Blocks.bedrock.setHardness(250.0F).setHarvestLevel("pickaxe", 3);;
+		Blocks.bedrock.setHardness(250.0F).setHarvestLevel("pickaxe", 3);
 	}
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onHarvestBedrock(PlayerInteractEvent event) {
+        Block block = event.world.getBlock(event.x, event.y, event.z);
+        if (event.action == PlayerInteractEvent.Action.LEFT_CLICK_BLOCK && block == Blocks.bedrock && event.y == 0) {
+            event.setCanceled(true);
+        }
+    }
 }
