0f795ea1ddba7168f5ab1d6552cc93082971bf25
aksource <akira.mew@gmail.com>
2014-08-06 00:32:28 +0900

    optimize import

--- java/DigBedrock/DigBedrock.java
+++ java/DigBedrock/DigBedrock.java
@@ -1,10 +1,8 @@
 package DigBedrock;
 
-import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.eventhandler.Event;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
