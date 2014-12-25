25756709f7eaa28b62af42d4cd7d656cd249af7f
aksource <akira.mew@gmail.com>
2014-12-25 21:05:30 +0900

    Bump up 1.8

--- java/DigBedrock/DigBedrock.java
+++ java/DigBedrock/DigBedrock.java
@@ -1,9 +1,9 @@
 package DigBedrock;
 
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.common.MinecraftForge;
@@ -11,7 +11,6 @@ import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 @Mod(modid="DigBedrock", name="DigBedrock", version="@VERSION@",dependencies="required-after:FML")
-
 public class DigBedrock
 {
 	@Mod.Instance("DigBedrock")
@@ -30,19 +29,19 @@ public class DigBedrock
 
     @SubscribeEvent
     public void onHarvestBedrock(PlayerInteractEvent event) {
-        Block block = event.world.getBlock(event.x, event.y, event.z);
-        if (event.action == PlayerInteractEvent.Action.LEFT_CLICK_BLOCK && block == Blocks.bedrock && event.y == 0) {
+        Block block = event.world.getBlockState(event.pos).getBlock();
+        if (event.action == PlayerInteractEvent.Action.LEFT_CLICK_BLOCK && block == Blocks.bedrock && event.pos.getY() == 0) {
             event.setCanceled(true);
         }
     }
 
     @SubscribeEvent
     public void digSpeed(PlayerEvent.BreakSpeed event) {
-        if (event.block == Blocks.bedrock) {
-            if (event.y < 40 && event.y > 0) {
-                event.newSpeed = event.originalSpeed * event.y;
+        if (event.state.getBlock() == Blocks.bedrock) {
+            if (event.pos.getY() < 40 && event.pos.getY() > 0) {
+                event.newSpeed = event.originalSpeed * event.pos.getY();
             }
-            if (event.y >= 40) {
+            if (event.pos.getY() >= 40) {
                 event.newSpeed = event.originalSpeed * 40;
             }
         }
