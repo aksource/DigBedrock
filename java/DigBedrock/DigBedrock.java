c0031624416d5ce3eab6974cadbf2e5d730dbb46
aksource <akira.mew@gmail.com>
2016-06-15 23:42:16 +0900

    test

--- java/DigBedrock/DigBedrock.java
+++ java/DigBedrock/DigBedrock.java
@@ -1,5 +1,6 @@
 package DigBedrock;
 
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -10,12 +11,17 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
-@Mod(modid="DigBedrock", name="DigBedrock", version="@VERSION@",dependencies="required-after:FML")
-public class DigBedrock
-{
-	@Mod.Instance("DigBedrock")
-	public static DigBedrock instance;
-	
+@Mod(modid=DigBedrock.MOD_ID,
+        name=DigBedrock.MOD_NAME,
+        version=DigBedrock.MOD_VERSION,
+        acceptedMinecraftVersions = DigBedrock.MOD_MC_VERSION)
+public class DigBedrock {
+
+    public static final String MOD_ID = "DigBedrock";
+    public static final String MOD_NAME = "DigBedrock";
+    public static final String MOD_VERSION = "@VERSION@";
+    public static final String MOD_MC_VERSION = "@VERSION@";
+
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
@@ -38,8 +44,19 @@ public class DigBedrock
     }
 
     @SubscribeEvent
+    public void breakBedRockEvent(BlockEvent.BreakEvent event) {
+        Block block = event.state.getBlock();
+        if (block == Blocks.bedrock && event.pos.getY() == 0) {
+            event.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent
     public void digSpeed(PlayerEvent.BreakSpeed event) {
         if (event.state.getBlock() == Blocks.bedrock) {
+            if (event.pos.getY() <= 0) {
+                event.setCanceled(true);
+            }
             if (event.pos.getY() < 40 && event.pos.getY() > 0) {
                 event.newSpeed = event.originalSpeed * event.pos.getY();
             }
